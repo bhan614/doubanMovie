6e51626d055a1b9229430a35ddd5677bea31c465
bhan614 <985900385@qq.com>
2017-05-07 20:06:58 +0800

    add header ui

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,27 @@
 <template>
   <div id="app">
+    <Dheader></Dheader>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Dheader from './header.vue'
 export default {
-  name: 'app'
+  name: 'app',
+  data() {
+    return {}
+  },
+  components: {
+    Dheader
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="less" rel="stylesheet/less">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 </style>
